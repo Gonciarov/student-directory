28266dfe86e388cbd58319c3ef9e22190a7f15b5
gonciarov <gonciarov@yandex.ru>
2020-06-30 10:33:06 +0100

    i

--- directory.rb
+++ directory.rb
@@ -1,4 +1,15 @@
 
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
+
+def print(names)
+  names.each do |name|
+    puts name
+  end
+end
+
 students = ["The students of Villains Academy", "Dr. Hannibal Lecter", "Darth Vader",
   "Nurse Ratched", "Michael Corleone", "Alex DeLarge",
   "The Wicked Witch of the West", "Terminator",
@@ -9,4 +20,10 @@ students = ["The students of Villains Academy", "Dr. Hannibal Lecter", "Darth Va
       puts student
   end
 
-print "Overall, we have #{students.count} great students"
+  def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+
+print_header
+print(students)
+print_footer(students)
