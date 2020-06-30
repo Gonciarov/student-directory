b7f6bf1f198d220b328f18dd9bc13f3b49664b2b
gonciarov <gonciarov@yandex.ru>
2020-06-30 09:38:43 +0100

    i

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+student_count = 11
 puts "The students of Villains Academy"
 puts "----------"
 puts "Dr. Hannibal Lecter"
@@ -12,5 +13,5 @@ puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
 print "Overall, we have "
-print "11 " 
-puts "great students"
+print student_count
+puts " great students"
