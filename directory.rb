cb2588d4c5b5eaeeaa966e94b369519afaf05782
gonciarov <gonciarov@yandex.ru>
2020-07-04 09:56:31 +0100

    added save_students method

--- directory.rb
+++ directory.rb
@@ -1,34 +1,75 @@
 
+@students = []
+
+def save_students
+  file = File.open("students.csv", "w")
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit the return twice"
+  name = gets.chomp
+  while !name.empty? do
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
+    name = gets.chomp
+    end
+  end
+
+  def interactive_menu
+    loop do
+      print_menu
+      process(gets.chomp)
+    end
+  end
+
+  def print_menu
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "3. Save"
+    puts "9. Exit"
+  end
+
+  def show_students
+    print_header
+    print_list
+    print_footer
+  end
+
+  def process(selection)
+  case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "3"
+      save_students
+    when "9"
+      exit # this will cause the program to terminate
+    else
+      puts "I don't know what you meant, try again"
+    end
+  end
+
+  def print_list
+    @students.each do |student|
+      puts "#{student[:name]} (#{student[:cohort]} cohort)"
+    end
+  end
+
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
 end
 
-def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]})"
-  end
+  def print_footer
+  puts "Overall, we have #{@students.count} great students"
 end
 
-students = [
-{name: "The students of Villains Academy", cohort: :november},
-{name: "Dr. Hannibal Lecter", cohort: :november},
-{name: "Darth Vader", cohort: :november},
-{name: "Nurse Ratched", cohort: :november},
-{name: "Michael Corleone", cohort: :november},
-{name: "Alex DeLarge", cohort: :november},
-{name: "The Wicked Witch of the West", cohort: :november},
-{name: "Terminator", cohort: :november},
-{name: "Freddy Krueger", cohort: :november},
-{name: "The Joker", cohort: :november},
-{name: "Joffrey Baratheon", cohort: :november},
-{name: "Norman Bates", cohort: :november}
-]
-
-  def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
-end
-
-print_header
-print(students)
-print_footer(students)
+interactive_menu
